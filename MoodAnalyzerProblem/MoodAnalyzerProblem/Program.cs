306884f189b59a2489a93439ad581e19af51eac4
prashantmagare <prashantmagare3010@gmail.com>
2022-06-02 00:19:05 +0530

    "Added - UC1_MoodAnalyzer"

--- MoodAnalyzerProblem/MoodAnalyzerProblem/Program.cs
+++ MoodAnalyzerProblem/MoodAnalyzerProblem/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
-
 namespace Mood_Analyzer_problem
 {
-    class Program
+    public enum Mood
     {
-        static void Main(string[] args)
-        {
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        Happy,
+        sad
+    }
 
+    public class MoodAnalyzer
+    {
+        public string CheckMood(string msg)
+        {
+            //msg : Raj is Happy for her new Mobile
+            //msg : Raj is Sad after getting Fail In Exam
+            if (msg.Contains("happy", StringComparison.OrdinalIgnoreCase))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
         }
     }
+
+
+
 }
